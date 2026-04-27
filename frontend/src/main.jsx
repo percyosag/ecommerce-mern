87ec5108f892b9ab1ad3238342def59cbd4c7718
Percy Osunde <posunde@my.centennialcollege.ca>
2026-04-27 11:59:38 -0400

    Frontend Milestone 4: Product Detail screen and dynamic routing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,11 +10,13 @@ import "bootstrap/dist/css/bootstrap.min.css"; // Core Bootstrap CSS
 import "./index.css"; // Your Tech Elite Dark Mode CSS
 import App from "./App.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
+import ProductScreen from "./screens/ProductScreen.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>,
   ),
 );
